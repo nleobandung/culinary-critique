c54e03799904b357eaf99d5b58aa57ca4e57beda
Bryan Mui <bryanmui94582@gmail.com>
2024-05-21 18:47:15 -0700

    added import header and modified app.js to output sample login

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import bplate1 from "./bplate1.jpg";
 import DiningHallsPage from "./DiningHallsPage";
+import SignUpPage from "./Components/Login/SignUpPage"
 
 function App() {
   const [rating, setRating] = useState(0);
@@ -21,38 +22,39 @@ function App() {
   };
 
   return (
-    <div className="App">
-      {currentPage === "profile" && (
-        <header className="App-header">
-          <h1>Bruin Plate</h1>
-          <div className="rating">
-            {[1, 2, 3, 4, 5].map((value) => (
-              <button
-                key={value}
-                className={value <= rating ? "active" : ""}
-                onClick={() => handleRatingChange(value)}
-              >
-                ★
-              </button>
-            ))}
-          </div>
-          <p className="number-ratings">{numRatings} ratings</p>
-          <div className="image-grid">
-            {/* Must find way to load multiple images from backend */}
-            <div className="image-item">
-              <img src={bplate1} alt="Dog 1" />
-              <p>Bruin Plate</p>
-            </div>
-            {/* Add more image items */}
-          </div>
-          <button onClick={switchToDiningHallsPage}>View Dining Halls</button>
-        </header>
-      )}
-      {currentPage === "diningHalls" && (
-        <DiningHallsPage switchToProfilePage={switchToProfilePage} />
-      )}
-    </div>
-  );
+    <SignUpPage/>
+  //   <div className="App">
+  //     {currentPage === "profile" && (
+  //       <header className="App-header">
+  //         <h1>Bruin Plate</h1>
+  //         <div className="rating">
+  //           {[1, 2, 3, 4, 5].map((value) => (
+  //             <button
+  //               key={value}
+  //               className={value <= rating ? "active" : ""}
+  //               onClick={() => handleRatingChange(value)}
+  //             >
+  //               ★
+  //             </button>
+  //           ))}
+  //         </div>
+  //         <p className="number-ratings">{numRatings} ratings</p>
+  //         <div className="image-grid">
+  //           {/* Must find way to load multiple images from backend */}
+  //           <div className="image-item">
+  //             <img src={bplate1} alt="Dog 1" />
+  //             <p>Bruin Plate</p>
+  //           </div>
+  //           {/* Add more image items */}
+  //         </div>
+  //         <button onClick={switchToDiningHallsPage}>View Dining Halls</button>
+  //       </header>
+  //     )}
+  //     {currentPage === "diningHalls" && (
+  //       <DiningHallsPage switchToProfilePage={switchToProfilePage} />
+  //     )}
+  //   </div>
+   );
 }
 
 export default App;
