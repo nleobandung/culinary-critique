479f5ded622cd0772b5ab58ba2cc6a302553d404
nleobandung <nathan.leobandung@gmail.com>
2024-05-31 21:26:27 -0700

    fixed login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import './LoginPage.css';
+import { loginUser, createUser } from '../api.js';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserDataContext } from '../context/UserDataProvider.js';
 
 function LoginPage() {
+  const { setUserDataState } = useContext(UserDataContext);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await loginUser({ username, password });
+      if (user) {
+        setUserDataState(true, username);
+        navigate('/');
+      }
+    } catch (error) {
+      setError('Invalid username or password');
+      console.error('Error logging in:', error);
+    }
+  };
+
+  const handleRegister = async () => {
+    const newUser = { name: username, password: password };
+
+    try {
+      await createUser(newUser);
+      setUserDataState(true, username); // Automatically log in the new user
+      navigate('/');
+    } catch (error) {
+      if (error.message.includes('Username already exists')) {
+        setError('Username already exists');
+      } else {
+        console.error('Error creating user:', error);
+        setError('Error creating user');
+      }
+    }
+  };
+
   return (
-    <div className="login-page">
-      <div className="container">
-        <div className="logo">
-          <h1>Culinary Critique</h1>
-        </div>
-        <form>
-          <div className="form-group">
-            <label>Username:</label>
-            <input type="text" name="username" />
-          </div>
-          <div className="form-group">
-            <label>Password:</label>
-            <input type="password" name="password" />
-          </div>
-          <div className="error">Error creating user</div>
-          <button type="submit">Login</button>
-          <button type="button">Register</button>
-        </form>
-      </div>
+    <div className="login-container">
+      <div className="login-logo">CULINARY CRITIQUE</div>
+      <form className="login-form" onSubmit={handleSubmit}>
+      <label className="login-label">
+        Username:
+        <input className="login-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label className="login-label">
+        Password:
+          <input className="login-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        {error && <div className="login-error">{error}</div>}
+        <button className="login-button" type="submit">Login</button>
+        <button className="login-button" type="button" onClick={handleRegister}>Register</button>
+      </form>
+      <Link to="/profile">Go to Profile</Link>
     </div>
   );
 }
