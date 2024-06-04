86e82624aece9c5ecdbe41d1c239b7e9a71f42df
Bryan Mui <bryanmui94582@gmail.com>
2024-06-03 22:58:00 -0700

    fixed navbar bug

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,20 +1,57 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from "react-router-dom";
-import "./NavBar.css";
+import './NavBar.css';
+import { getProfileNames } from '../api.js';
+import SearchResults from './SearchResults';
 import { UserDataContext } from "../context/UserDataProvider"
 
-const Navbar = () => {
+function NavBar() {
   const { userData } = useContext(UserDataContext);
 
+  const [searchQuery, setSearchQuery] = useState("");
+  const [profileNames, setProfileNames] = useState([]);
+  const [filteredProfiles, setFilteredProfiles] = useState([]);
+
+  useEffect(() => {
+    fetchProfileNames();
+  }, []);
+
+  const fetchProfileNames = async () => {
+    try {
+      const names = await getProfileNames();
+      setProfileNames(names);
+    } catch (error) {
+      console.error('Error fetching profile names:', error);
+    }
+  };
+
+  const handleSearch = (event) => {
+    const input = event.target.value;
+    setSearchQuery(input);
+
+    if (input.length === 0) {
+      setFilteredProfiles([]);
+    }
+
+    if (input.length > 0) {
+      const results = profileNames.filter(profile =>
+        profile.toLowerCase().includes(input.toLowerCase())
+      );
+      setFilteredProfiles(results);
+    }
+  };
+
   return (
-    <div className="nav">
-      <div className="nav-logo">Culinary-Critique</div>
+    <nav className="nav">
+      <div className="nav-logo">
+        <Link to="/">Culinary Critique</Link>
+      </div>
       <ul className="nav-menu">
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/create-food">Add Food Location</Link>
+          <Link to="/create-food">Explore</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
@@ -26,9 +63,9 @@ const Navbar = () => {
             <Link to="/login" className="nav-profile">Log In</Link>
           )}
         </div>
-      </ul>
-    </div>
+        </ul>
+    </nav>
   );
-};
+}
 
-export default Navbar;
+export default NavBar;
