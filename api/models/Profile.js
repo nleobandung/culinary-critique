a2fdd741642ca024f4c224657827993c8c1c2204
nleobandung <nathan.leobandung@gmail.com>
2024-06-07 01:20:49 -0700

    added array of pointers to comments for users in database:

--- api/models/Profile.js
+++ api/models/Profile.js
@@ -1,24 +1,19 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import Comment from './Comment.js'
 
 dotenv.config();
 
 const ratingSchema = new mongoose.Schema({
     stars: { type: Number, required: true, min: 1, max: 5},
     username: { type: String, required: true}
-})
-
-const commentSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    text: { type: String, required: true },
-    date: { type: Date, default: Date.now }
-})
+});
 
 const profileSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true},
     imageLink: { type: String, default: `https://${process.env.S3_BUCKET}.s3.${process.env.REGION}.amazonaws.com/dog.jpg`},
     ratings: { type: [ratingSchema], default: [] },
-    comments: { type: [commentSchema], default: [] }
+    comments: { type: [Comment.schema], default: [] }
 });
 
 profileSchema.virtual('numberOfRatings').get(function() {
