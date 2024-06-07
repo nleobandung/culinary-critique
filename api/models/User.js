f5ee578194434dd08e19c4bb9b72220dc7b3524a
Bryan Mui <bryanmui94582@gmail.com>
2024-06-07 15:51:51 -0700

    cleaned up code for final shipping

--- api/models/User.js
+++ api/models/User.js
@@ -3,18 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const followerSchema = new mongoose.Schema({
-    followerID: {type: String, default: ""},
-    status: {type: Number, default: -1}
-});
-
 const userSchema = new mongoose.Schema({
 
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     profilePhoto: { type: String, default: `https://${process.env.S3_BUCKET}.s3.${process.env.REGION}.amazonaws.com/dog.jpg`},
     comments: { type: [mongoose.Schema.Types.ObjectId], default: []},
-    followers: {type: [followerSchema]}
 });
 
 
